93baf90cbf433edd7162ed126478b7d800d11482
Aniket Sedhai <asedhai@masonite.com>
2024-07-11 12:45:03 -0500

    Initial Commit

---
+++ Stack/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Stack
+//
+//  Created by Aniket Sedhai on 7/11/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
